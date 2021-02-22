b3060be5cb7163fd26f508afda21f55bca4e143d
sixmonek <sixmontoja@gmail.com>
2021-02-22 14:23:49 +0100

    Create neko.js

---
+++ neko.js
@@ -0,0 +1,30 @@
+const { Console } = require("console");
+const Discord = require("discord.js");
+const superagent = require("superagent");
+
+module.exports.run = async (_client, message) => {
+
+    var channel = message.channel;
+
+    let {body} = await superagent
+    .get('https://nekobot.xyz/api/image?type=neko').catch((err) => {
+        channel.send("Błąd z API!")
+        return Console.log(err);
+    });
+    
+   try{
+       let embed = new Discord.MessageEmbed()
+       .setColor(body.color)
+       .setImage(body.message);
+       channel.send(embed);
+    } catch(err){
+        channel.send("Wystąpił bląd z botem napisz do Ownera!")
+        return Console.log(err);
+    }
+
+    
+}
+
+module.exports.help = {
+    name: "neko"
+}
